1954a020aa1dd9192e51444deae9f67eb8af4aac
Arpit Patel <delhiarpitpatel@gmail.com>
2026-03-08 17:40:03 +0530

    codecrafters submit [skip ci]

--- app/main.php
+++ app/main.php
@@ -5,8 +5,9 @@ while(true){
     fwrite(STDOUT, "$ ");
     $command = rtrim(fgets(STDIN), "\r\n");
     if($command === 'exit') break;
-    if(strpos($command,'echo') === 0){
-        fwrite(STDOUT, str_replace('echo ', '', $command, 1));
+    if(str_starts_with($command, 'echo ')){
+        fwrite(STDOUT, substr($command, 5) . "\n");
+        continue;
     }
     fwrite(STDOUT, "{$command}: command not found\n");
 }
---
+++ php
@@ -0,0 +1,1 @@
+echo AA.strpos("echo aaa","echo"); -V
