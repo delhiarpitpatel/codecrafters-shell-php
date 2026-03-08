494397a46025a8a80c3b5797eb740fc064c1b928
Arpit Patel <delhiarpitpatel@gmail.com>
2026-03-08 17:30:28 +0530

    codecrafters submit [skip ci]

--- app/main.php
+++ app/main.php
@@ -1,7 +1,8 @@
 <?php
 error_reporting(E_ALL);
 
-// TODO: Uncomment the code below to pass the first stage
-fwrite(STDOUT, "$ ");
-$command = rtrim(fgets(STDIN), "\r\n");
-fwrite(STDOUT, "{$command}: command not found");
+while(true){
+    fwrite(STDOUT, "$ ");
+    $command = rtrim(fgets(STDIN), "\r\n");
+    fwrite(STDOUT, "{$command}: command not found");
+}
