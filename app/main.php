8e81c106969fa31f01e963cbdb7bd08dc5425477
Arpit Patel <delhiarpitpatel@gmail.com>
2026-03-08 18:15:36 +0530

    codecrafters submit [skip ci]

--- app/main.php
+++ app/main.php
@@ -22,22 +22,20 @@ while(true){
         continue;
     }
     switch($command){
-        case 'exit':
-            break 2;
+        case 'exit': break 2;
         case 'echo':
             fwrite(STDOUT, implode(' ',$args) . "\n");
             break;
+        case 'pwd':
+            fwrite(STDOUT, getcwd() . "\n");
+            break;
         case 'type':
-            $cmd = $args[0];
-            if(in_array($cmd, $commands)){
+            if(in_array($cmd = $args[0], $commands))
                 fwrite(STDOUT, $cmd . " is a shell builtin\n");
-                break;
-            } 
-            if($full_path = get_executable_path($cmd)){
+            else if($full_path = get_executable_path($cmd))
                 fwrite(STDOUT, $cmd. " is ". $full_path . "\n");
-                break;
-            }
-            fwrite(STDOUT, $cmd . ": not found\n");
+            else
+                fwrite(STDOUT, $cmd . ": not found\n");
             break;
         default:
             fwrite(STDOUT, "{$command}: command not found\n");
