6fd866312d280309bae9d2daf2d033049712064e
Arpit Patel <delhiarpitpatel@gmail.com>
2026-03-08 17:44:29 +0530

    codecrafters submit [skip ci]

--- app/main.php
+++ app/main.php
@@ -1,12 +1,23 @@
 <?php
 error_reporting(E_ALL);
 
+$commands = [
+    'echo', 'type', 'exit'
+];
 while(true){
     fwrite(STDOUT, "$ ");
     $command = rtrim(fgets(STDIN), "\r\n");
     if($command === 'exit') break;
     if(str_starts_with($command, 'echo ')){
-        fwrite(STDOUT, substr($command, 5) . "\n");
+        $arg = substr($command, 5);
+        fwrite(STDOUT, $arg . "\n");
+    } else if( str_starts_with($command, 'type ')){
+        $arg = substr($command, 5);
+        if(in_array($arg, $commands)){
+            fwrite(STDOUT, $arg . " is a shell builtin\n");
+        } else{
+            fwrite(STDOUT, $arg . ": not found\n");
+        }
     } else {
         fwrite(STDOUT, "{$command}: command not found\n");
     }
