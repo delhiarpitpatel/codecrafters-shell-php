b513274be83f0b691bb98824ab6a6eb5b201f515
Arpit Patel <delhiarpitpatel@gmail.com>
2026-03-08 18:09:11 +0530

    codecrafters submit [skip ci]

--- app/main.php
+++ app/main.php
@@ -4,27 +4,43 @@ error_reporting(E_ALL);
 $commands = [
     'echo', 'type', 'exit'
 ];
+function get_executable_path($cmd){
+    foreach ( explode(':', $_SERVER['PATH']) as $path){
+        if(file_exists($full_path = $path. DIRECTORY_SEPARATOR. $cmd) and is_executable($full_path)){
+            return $full_path;
+        }
+    }
+    return false;
+}
 while(true){
     fwrite(STDOUT, "$ ");
-    $command = rtrim(fgets(STDIN), "\r\n");
-    if($command === 'exit') break;
-    if(str_starts_with($command, 'echo ')){
-        $arg = substr($command, 5);
-        fwrite(STDOUT, $arg . "\n");
-    } else if( str_starts_with($command, 'type ')){
-        $cmd = substr($command, 5);
-        if(in_array($cmd, $commands)){
-            fwrite(STDOUT, $cmd . " is a shell builtin\n");
-            continue;
-        } 
-        foreach ( explode(':', $_SERVER['PATH']) as $path){
-            if(file_exists($full_path = $path. DIRECTORY_SEPARATOR. $cmd) and is_executable($full_path)){
+    $input_command = rtrim(fgets(STDIN), "\r\n");
+    $args = explode(' ', $input_command);
+    $command = array_shift($args);
+    if(get_executable_path($command)){
+        exec($command . ' '. $input_command); 
+        continue;
+    }
+    switch($command){
+        case 'exit':
+            break 2;
+        case 'echo':
+            fwrite(STDOUT, implode(' ',$args) . "\n");
+            break;
+        case 'type':
+            $cmd = $args[0];
+            if(in_array($cmd, $commands)){
+                fwrite(STDOUT, $cmd . " is a shell builtin\n");
+                break;
+            } 
+            if($full_path = get_executable_path($cmd)){
                 fwrite(STDOUT, $cmd. " is ". $full_path . "\n");
-                continue 2;
+                break;
             }
-        }
-        fwrite(STDOUT, $cmd . ": not found\n");
-    } else {
-        fwrite(STDOUT, "{$command}: command not found\n");
+            fwrite(STDOUT, $cmd . ": not found\n");
+            break;
+        default:
+            fwrite(STDOUT, "{$command}: command not found\n");
+            break;
     }
 }
