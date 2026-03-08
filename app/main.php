2b87a4ee88486eca94ac406e067a2b8b85135ac8
Arpit Patel <delhiarpitpatel@gmail.com>
2026-03-08 17:31:28 +0530

    codecrafters submit [skip ci]

--- app/main.php
+++ app/main.php
@@ -4,5 +4,6 @@ error_reporting(E_ALL);
 while(true){
     fwrite(STDOUT, "$ ");
     $command = rtrim(fgets(STDIN), "\r\n");
+    if($command === 'exit') break;
     fwrite(STDOUT, "{$command}: command not found\n");
 }
