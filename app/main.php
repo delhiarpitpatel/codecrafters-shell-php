a4e345b7ffc491daeba80dc36273b4cc7f329e15
Arpit Patel <delhiarpitpatel@gmail.com>
2026-03-08 17:55:06 +0530

    codecrafters submit [skip ci]

--- app/main.php
+++ app/main.php
@@ -12,12 +12,18 @@ while(true){
         $arg = substr($command, 5);
         fwrite(STDOUT, $arg . "\n");
     } else if( str_starts_with($command, 'type ')){
-        $arg = substr($command, 5);
-        if(in_array($arg, $commands)){
-            fwrite(STDOUT, $arg . " is a shell builtin\n");
-        } else{
-            fwrite(STDOUT, $arg . ": not found\n");
+        $cmd = substr($command, 5);
+        if(in_array($cmd, $commands)){
+            fwrite(STDOUT, $cmd . " is a shell builtin\n");
+            continue;
+        } 
+        foreach ( explode(':', $_SERVER['PATH']) as $path){
+            if(file_exists($full_path = $path. DIRECTORY_SEPARATOR. $cmd) and is_executable($full_path)){
+                fwrite(STDOUT, $cmd. " is ". $full_path . "\n");
+                continue 2;
+            }
         }
+        fwrite(STDOUT, $cmd . ": not found\n");
     } else {
         fwrite(STDOUT, "{$command}: command not found\n");
     }
